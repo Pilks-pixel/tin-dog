9facf72fad377a4664c13cfdd0466dccb6c86542
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2023-03-08 16:00:01 +0700

    eventListeners

--- dogs.js
+++ dogs.js
@@ -1,32 +1,45 @@
-
 class Dog {
-    constructor(dog) {
-        Object.assign(this, dog)
-    }
+	constructor(dog) {
+		Object.assign(this, dog);
+	}
 
-    speak() {
-        console.log(`Hello my name is ${this.name}`)
-    }
+	speak() {
+		console.log(`Hello my name is ${this.name}`);
+	}
 
-    getDogHtml() {
+	getDogHtml(result) {
+		const { avatar, name, age, bio, hasBeenSwiped } = this;
+		const status = hasBeenSwiped ? this.setLikedStatus(result) : "";
 
-        const {avatar, name, age, bio} = this;
-
-        return ` 
-            <div>
-            <img class='profile__avatar' src='../${avatar}' alt='a dog' height="250px" width="auto" >
-            <p class='profile__heading'>${name} ${age}</p>
-            <p class='profile__bio'>${bio}</p>
+		return ` 
+            <div class='profile__card'>
+            <img class='card__avatar' src='../${avatar}' alt='a dog' height="250px" width="auto" >
+            ${status}
+            <p class='card__heading'>${name} ${age}</p>
+            <p class='card__bio'>${bio}</p>
             </div>
-        `
-    }
+        `;
+	}
 
-    setLiked() {
-        return this.hasBeenLiked = true
-    }
+	setLikedStatus(result) {
+		let status;
 
+		if (result == true) {
+			this.hasBeenLiked = true;
+			status = "like-image.png";
+		} else {
+			this.hasBeenLiked = false;
+			status = "nope-image.png";
+		}
 
-    
+		return `
+        <img class='card__avatar' src='../assets/${status}' alt='status' height="250px" width="auto" >
+        `;
+	}
+
+	setSwiped() {
+		return (this.hasBeenSwiped = true);
+	}
 }
 
-export { Dog }
+export { Dog };
--- index.js
+++ index.js
@@ -3,18 +3,33 @@ import { Dog } from "./dogs.js";
 
 
 // To Do 
-
-//add onClick event listener and function to handle dog click states
-// implement logic for adding like or nope image
-    
+// Render dogs from an Array
+// On click change to next Dog
+// Handle end of dogs
 
 const puppy = new Dog(dogs[0]);
 
-function render () {
-     document.getElementById('profile').innerHTML = puppy.getDogHtml()
+
+
+
+function handleSwipe(e) {
+     let result = e.currentTarget.id == 'like'
+     puppy.setSwiped()
+     console.log({result}, puppy, document.body)
+     render(result)
+
+}
+
+document.querySelector('.btn--reject').addEventListener('click', handleSwipe )
+document.querySelector('.btn--heart').addEventListener('click', handleSwipe )
+
+
+function render (result) {
+     document.getElementById('profile').innerHTML = puppy.getDogHtml(result)
 }
 
 render();
+
 
 
 
